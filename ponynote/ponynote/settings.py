0f602703ac75186c7d753e813e3db6922a699bd1
Yotam Manor <yotammanor@gmail.com>
2018-04-25 17:45:57 +0300

    add webpack_loader to settings

--- ponynote/ponynote/settings.py
+++ ponynote/ponynote/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webpack_loader',
 ]
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'bundles/',
+            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
+        }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
